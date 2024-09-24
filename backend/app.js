200f706863161317ec9ec09e3251c972cae3e28a
tunanocrust <ewhuet@gmail.com>
2024-09-24 15:16:19 +0300

    - setup the basic express app to read from the views directory and use the indexRouter

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('node:path')
+const dotenv = require('dotenv')
+const {indexRouter} = require('./routes/indexRouter')
+
+const app = express()
+
+//set up the absolute path to use the variables stored in the .env file
+dotenv.config({path: path.resolve(__dirname, '../.env')})
+
+//Sets the directory where view (template) files are stored.
+app.set('views', path.join(__dirname, '../frontend', 'views'))
+app.set('view engine', 'ejs')
+
+//parses incoming data from a post request and makes it available to the req.body
+app.use(express.urlencoded({extended: true}))
+
+//defines the css path and makes them available to be served
+const assetsPath = path.join(__dirname, '../frontend', 'public')
+app.use(express.static(assetsPath))
+
+
+app.use('/', indexRouter)
+
+const PORT = process.env.PORT
+app.listen(PORT, console.log(`Server running on ${PORT}`))
+
