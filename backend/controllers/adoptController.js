40470cbde70bb7a0cf6f26592926715946cad67d
tunanocrust <ewhuet@gmail.com>
2024-10-13 21:48:30 +0300

    changed adopt frontend and backend - imported AnimalClass, - created new class for AdoptController to hold all the functions for the adopt pages - refactored to query the postgresql database

--- backend/controllers/adoptController.js
+++ backend/controllers/adoptController.js
@@ -1,26 +1,42 @@
 const queries = require('../db/queries')
+const {AnimalClass} = require('../db/queries')
 
-async function getAllItems (req, res) {
-    const items = await queries.getAllItems();
-    console.log(items)
-    res.render('adopt', {
-        title: "All Animals",
-        intro: "Meet the Paws That Need a Home!",
-        items: items
-    })
-}
 
-async function getSpecies (req, res) {
-    const speciesID = parseInt(req.params.id)
-    const animals = await queries.getBySpecies(speciesID)
-    res.render('adopt', {
-        title: animals[0].species,
-        intro: "Meet the Paws That Need a Home!",
-        items: animals
-    })
+class AdoptController {
+    async getAllAnimals (req, res) {
+        try {
+            const animals = await AnimalClass.getAllAnimals()
+            if(animals){
+                res.render('adopt', {
+                    title: "All Animals",
+                    intro: "Meet the Paws That Need a Home!",
+                    items: animals
+                })
+                
+            } else {
+                res.status(404).json({message: 'No animals found'})
+            }
+        } catch(err) {
+            res.status(500).json({error: 'Error fetching animals'})
+        }
+    }
+
+    async getBySpecies(req, res) {
+        const species = req.params.species
+        try {
+            const animals = await AnimalClass.getBySpecies(species)
+            res.render('adopt', {
+                title: animals[0].species,
+                intro: 'Meet the Paws That Need a Home!',
+                items: animals
+            })
+        } catch(err) {
+            console.error('Error finding animals: ', err);
+            res.status(500).send('Error finding animals')
+        }
+    }
 }
 
 module.exports = {
-    getAllItems,
-    getSpecies
+    AdoptController
 }
--- backend/routes/adoptRouter.js
+++ backend/routes/adoptRouter.js
@@ -1,14 +1,16 @@
 const {Router} = require('express');
 const adoptRouter = Router();
-const adoptController = require('../controllers/adoptController')
+// const adoptController = require('../controllers/adoptController')
+const {AdoptController} = require('../controllers/adoptController')
 
+const adoptController = new AdoptController()
 
-adoptRouter.get('/' , async (req, res) => {
-    adoptController.getAllItems(req, res)
+adoptRouter.get('/', async (req, res) => {
+    adoptController.getAllAnimals(req, res)
 })
 
-adoptRouter.get('/:id', async (req, res) => {
-    adoptController.getSpecies(req, res)
+adoptRouter.get('/:species', async (req, res) => {
+    adoptController.getBySpecies(req, res)
 })
 
 module.exports = {
