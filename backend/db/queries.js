d3d65ce7aff7edda5ca9e7ae119a3d0a97462ed7
tunanocrust <ewhuet@gmail.com>
2024-10-13 21:53:23 +0300

    removed queries for the test database and added queries for the postgres db

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -1,30 +1,68 @@
-const db = require('../data/animalData')
+const db = require('../data/animalData');
+const pool = require('./pool');
 
-async function getAllItems() {
-    const dogData = db.dogData
-    const catData = db.catData
-    const allItems = dogData.concat(catData)
-    return allItems;
+class FormData {
+    async getHobbies(){
+        const query = 'SELECT hobby_name FROM Hobbies'
+        try{
+            const res = await pool.query(query)
+            return res.rows
+        } catch(err){
+            console.error('Error fetching hobby options: ', err)
+            throw err;
+        }
+    }
 }
 
-async function getBySpecies(id) {
-    const dogData = db.dogData
-    const catData = db.catData
-    const allItems = dogData.concat(catData)
-    const allSpeciies = allItems.filter( species => species.speciesID === id)
-    return allSpeciies;
-}
+class AnimalClass {
+    static async createAnimal(name, species, age, gender, weight, img){
+        const query = 'INSERT INTO Animals (name, species, age, gender, weight, img) VALUES ($1, $2, $3, $4, $5, $6)'
+        const values = [name, species, age, gender, weight, img]
+        try {
+            await pool.query(query, values)
+        } catch(err){
+            console.error('Error creating animal:', err);
+            throw err;
+        }
+    }
 
-async function getItem(id) {
-    const dogData = db.dogData
-    const catData = db.catData
-    const allItems = dogData.concat(catData)
-    let animal = allItems.find( (animal) => animal.id === id)
-    return animal
+    static async getAllAnimals(){
+        const query = 'SELECT * FROM Animals'
+        try {
+            const res = await pool.query(query)
+            return res.rows
+        } catch(err) {
+            console.error('Error fetching all animals: ', err);
+            throw err;
+        }
+    }
+
+    static async getBySpecies(species) {
+        const query = 'SELECT * FROM Animals WHERE species = $1'
+        const value = [species]
+        try {
+            const res = await pool.query(query, value)
+            return res.rows
+        } catch(err) {
+            console.error('Error fetching species: ', err);
+            throw err
+        }
+    }
+
+    static async getAnimalByID(id){
+        const query = 'SELECT * FROM Animals WHERE animal_id = $1'
+        const value = [id]
+        try {
+            const res = await pool.query(query, value)
+            return res.rows[0]
+        } catch(err) {
+            console.error('Error fetching animal: ', err);
+            throw err;
+        }
+    }
 }
 
 module.exports = {
-    getAllItems,
-    getBySpecies,
-    getItem
+    FormData,
+    AnimalClass
 }
