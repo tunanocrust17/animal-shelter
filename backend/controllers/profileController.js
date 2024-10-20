681798cd200ea2ac23c50b2607517e46421ce16d
tunanocrust <ewhuet@gmail.com>
2024-10-20 22:44:20 +0300

    - added the hobbies and quirks queries to be displayed on the profile page - added the functionality to add hobbies and quirks when creating an animal

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -5,11 +5,17 @@ class ProfileController {
         const animalID = parseInt(req.params.id)
 
         try {
+            //Get animal information
             const animal = await AnimalClass.getAnimalByID(animalID)
+            const hobbies = await AnimalClass.getAnimalHobbies(animalID)
+            const quirks = await AnimalClass.getAnimalQuirks(animalID)
+            console.log(quirks)
             res.render('animalProfile', {
                 title: animal.name,
                 intro: animal.about,
-                animal: animal
+                animal: animal,
+                hobbies: hobbies,
+                quirks: quirks
             })
         } catch(error){
             console.error('Error finding animal: ', error);
